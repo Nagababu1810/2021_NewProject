7c0d4d336bb46b8f2709b9c99c8e9cd07e4f9bdd
NagababuLabs <nagababu.tester18@gmail.com>
2021-03-29 10:15:32 +0530

    First Line

---
+++ JavaPrograms/src/programs/Addtioin.java
@@ -0,0 +1,15 @@
+package programs;
+
+public class Addtioin {
+
+	public static void main(String[] args) {
+		
+		
+		int x=10, y=20, z=30;
+		
+		System.out.println("This is my first line");
+		
+
+	}
+
+}
